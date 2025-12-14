fd4994fd8cae58ad6aa20eecf81380228129d2c8
AaronRD005 <aronrd2005@gmail.com>
2025-12-14 21:41:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ if not GEMINI_API_KEY:
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("models/gemini-1.0-pro")
+
 
 
 # ===============================
